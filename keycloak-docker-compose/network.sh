342239cf3a785f9419d07f726e00e240e37db342
Deebendu <>
2022-11-19 20:53:28 +0530

    Docker compose 3.9 container_name: Setting the container name is not supported.

--- keycloak-docker-compose/network.sh
+++ keycloak-docker-compose/network.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 echo "Creating docker network"
-sudo docker network create --driver=bridge \
---subnet=192.168.10.0/24 \
---gateway=192.168.10.1 \
-zestic.internal
+sudo docker network create --scope=swarm \
+--driver=bridge \
+--subnet=172.1.1.0/24 \
+--gateway=172.1.1.1 \
+keycloak
 
-sudo docker network create --driver=bridge \
+sudo docker network create --scope=swarm \
+--driver=bridge \
 --subnet=192.168.50.0/24 \
 --gateway=192.168.50.1 \
 zestic.external
