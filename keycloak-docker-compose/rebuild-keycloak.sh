efb740c56f9beacfe5d2ab5c1a81be373e5b13a7
Deebendu <>
2022-11-19 20:29:32 +0530

    Updated

--- keycloak-docker-compose/rebuild-keycloak.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo docker-compose stop keycloak
-sudo docker-compose rm -f keycloak
-sudo docker-compose build keycloak
-sudo docker-compose up -d keycloak
--- keycloak-docker-compose/rebuild-nginx.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo docker-compose stop authy-nginx
-sudo docker-compose rm -f authy-nginx
-sudo docker-compose build authy-nginx
-sudo docker-compose up -d authy-nginx
