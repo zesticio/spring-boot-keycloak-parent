fbf326b412c607c92dd0d6857dec55af5fa8d255
Deebendu <>
2022-12-14 22:43:27 +0530

    Updated

--- keycloak-docker-compose/network.sh
+++ keycloak-docker-compose/network.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 echo "Creating docker network"
-sudo docker network create --scope=swarm \
---driver=bridge \
---subnet=172.1.1.0/24 \
---gateway=172.1.1.1 \
-keycloak
-
-sudo docker network create --scope=swarm \
---driver=bridge \
---subnet=192.168.50.0/24 \
---gateway=192.168.50.1 \
-zestic.external
-
-sudo docker network ls
+docker network create --driver=bridge --subnet=172.1.1.0/24 --gateway=172.1.1.1 keycloak
--- keycloak-docker-compose/sql/2-init-keycloak-db.sh
+++ keycloak-docker-compose/sql/2-init-keycloak-db.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
 psql -U dev -tc "SELECT 1 FROM pg_database WHERE datname = 'keycloak'" | grep -q 1 || psql -U dev -c "CREATE DATABASE keycloak"
-
 psql -U dev -c "CREATE USER keycloak WITH PASSWORD 'keycloakrw'"
